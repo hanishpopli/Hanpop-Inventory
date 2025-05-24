4371bde228905d9381c94c289d44b3c3c13bf572
hanishpopli <poplihanish@gmail.com>
2025-05-24 22:52:23 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hanpop Inventory Dashboard</h1>
+      <p>Welcome! This is your restaurant management panel.</p>
+    </div>
+  );
+}
